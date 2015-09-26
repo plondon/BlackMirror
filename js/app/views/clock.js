09ee47cf32d4d98dee6cdb1401033338df594eb9
Philip London <plondon@gm.slc.edu>
2015-09-26 09:35:48 -0400

    add date and ampm

--- js/app/views/clock.js
+++ js/app/views/clock.js
@@ -8,17 +8,28 @@ define([
 
 var ClockView = Backbone.View.extend({
 	initialize: function() {
+		this.today();
 		this.ticktock();
 		setInterval( this.ticktock.bind(this), 1000 );
+	},
+	today: function() {
+		var time = new Date();
+		time = time.toDateString();
+
+		this.$el.find('.date').html(time);
 	},
 	ticktock: function() {
 		var time = new Date();
 		var hour = time.getHours() % 12;
+		var ampm = time.getHours() < 12 ? 'AM' : 'PM';
 		var minute = this.addZero(time.getMinutes());
 		var seconds = this.addZero(time.getSeconds());
-		var timeString = hour + ':' + minute + ':' + seconds;
 
-		this.$el.html(timeString);
+		var timeString = hour + ':' + minute + ':' + seconds;
+		var $ampm = $('<span>').html(ampm);
+
+		this.$el.find('.time').html(timeString)
+			.append($ampm);
 	},
 	addZero: function(i) {
 		if (i < 10) {
